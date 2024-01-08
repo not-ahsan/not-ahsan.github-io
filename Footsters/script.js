80756781ffed2aeca4938fa5e00adcbee0096e96
Muhammad Ahsan <154927442+not-ahsan@users.noreply.github.com>
2024-01-09 02:51:02 +0400

    script.js

--- Footsters/script.js
+++ Footsters/script.js
@@ -1,9 +1,5 @@
 "use strict";
 
-/**
- * navbar toggle
- */
-
 const overlay = document.querySelector("[data-overlay]");
 const navOpenBtn = document.querySelector("[data-nav-open-btn]");
 const navbar = document.querySelector("[data-navbar]");
@@ -17,10 +13,6 @@ for (let i = 0; i < navElems.length; i++) {
     overlay.classList.toggle("active");
   });
 }
-
-/**
- * header & go top btn active on page scroll
- */
 
 const header = document.querySelector("[data-header]");
 const goTopBtn = document.querySelector("[data-go-top]");
